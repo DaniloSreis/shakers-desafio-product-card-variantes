2fcf7103b23674d9195e5814a1b2e7a7bed198ce
Danilo Santos <danilosreis12@gmail.com>
2026-03-08 16:53:37 -0300

    style: remove white spaces from js

--- assets/product-card.js
+++ assets/product-card.js
@@ -1,60 +1,63 @@
-  const colors = document.querySelectorAll(".product-card__color")
-  const sizes = document.querySelectorAll(".product-card__size")
-  const productCard= document.querySelector(".product-card")
-  const productImage = document.querySelector(".product-card__image")
-  const productPrice = document.querySelector(".product-card__price")
-  const productVariants = JSON.parse(productCard.dataset.variants)
-  
-  let clothes;
-  let currentSize = "p"
-  let currentColor = "preto"
-  function findOption() {
-    const options = currentSize + " / " + currentColor
-    const formatPrice = new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL"
-    })
-     clothes = productVariants.find((variant) => {
-      return variant.title.toLowerCase() === options.toLocaleLowerCase() 
-    })
-  
-    if (clothes) {
-      productImage.src = clothes.featured_image.src
-      productPrice.innerText = formatPrice.format(clothes.price / 100)
-    }
+const colors = document.querySelectorAll(".product-card__color");
+const sizes = document.querySelectorAll(".product-card__size");
+const productCard = document.querySelector(".product-card");
+const productImage = document.querySelector(".product-card__image");
+const productPrice = document.querySelector(".product-card__price");
+const productVariants = JSON.parse(productCard.dataset.variants);
+
+let clothes;
+let currentSize = "p";
+let currentColor = "preto";
+function findOption() {
+  const options = currentSize + " / " + currentColor;
+  const formatPrice = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  clothes = productVariants.find((variant) => {
+    return variant.title.toLowerCase() === options.toLocaleLowerCase();
+  });
+
+  if (clothes) {
+    productImage.src = clothes.featured_image.src;
+    productPrice.innerText = formatPrice.format(clothes.price / 100);
   }
-  findOption()
-  
-  function selectColor(e) {
-    colors.forEach((color) => color.classList.remove("product-card__color--selected"))
-    e.currentTarget.classList.add("product-card__color--selected")
-    currentColor = e.currentTarget.innerText.toLowerCase()
-    findOption()
-  } 
-  
-  function selectSize(e) {
-    sizes.forEach((size) => size.classList.remove("product-card__size--selected"))
-    e.currentTarget.classList.add("product-card__size--selected")
-    currentSize= e.currentTarget.innerText.toLowerCase()
-    findOption()
-  
-  } 
-  
-  colors.forEach((color) => color.addEventListener("click", selectColor))
-  sizes.forEach((size) => size.addEventListener("click", selectSize))
-  
-  async function addToCart(item) {
-    try {
-      const response = await fetch(window.Shopify.routes.root + "cart/add.js", {
-        method: "POST",
-        headers:{
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          items: [item],
-        }),
-      })
-    } catch(err) {
-      console.error(err)
-    }
+}
+findOption();
+
+function selectColor(e) {
+  colors.forEach((color) =>
+    color.classList.remove("product-card__color--selected"),
+  );
+  e.currentTarget.classList.add("product-card__color--selected");
+  currentColor = e.currentTarget.innerText.toLowerCase();
+  findOption();
+}
+
+function selectSize(e) {
+  sizes.forEach((size) =>
+    size.classList.remove("product-card__size--selected"),
+  );
+  e.currentTarget.classList.add("product-card__size--selected");
+  currentSize = e.currentTarget.innerText.toLowerCase();
+  findOption();
+}
+
+colors.forEach((color) => color.addEventListener("click", selectColor));
+sizes.forEach((size) => size.addEventListener("click", selectSize));
+
+async function addToCart(item) {
+  try {
+    const response = await fetch(window.Shopify.routes.root + "cart/add.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [item],
+      }),
+    });
+  } catch (err) {
+    console.error(err);
   }
+}
